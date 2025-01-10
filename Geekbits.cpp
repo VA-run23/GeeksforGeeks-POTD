946a33d15411330c34c33f180f3ea17911ba790b
VA-run23 <1523codes@gmail.com>
2025-01-10 17:05:07 +0530

    Calculate number of days required to get N Geekbits

---
+++ Geekbits.cpp
@@ -0,0 +1,26 @@
+//Program to calculate the number of days required to get n geekBits
+//BY : 23Neurons
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of geekBits you want : ";
+    cin >> n;
+    int x = 0;
+    int count = 0;
+    for(int i = 1;i<=n;i++){
+
+        x ++;count++;
+        if(i %8 ==0){
+            x = x+ 8;
+        }
+        if(x >=n){
+            break;
+        }
+
+    }
+    cout<<"To get"<<n<<" geekBits, you need to wait for "<<count<<" days"<<endl;
+
+    return 0;
+}
