a77354c1698d1302b4957dc984d4e6c98a781b8d
VA-run <1523codes@gmail.com>
2025-02-01 09:21:12 +0530

    Added solution for Feb 1

--- 2025_CPP_SOLUTIONS_POTD/02_Feb_2025/POTD_01_02_2025.cpp
+++ 2025_CPP_SOLUTIONS_POTD/02_Feb_2025/POTD_01_02_2025.cpp
@@ -1,1 +1,36 @@
+class Solution {
+  public:
+    bool isWordExist(vector<vector<char>>& mat, string& word) {
+        // Code here
+        int n = mat.size();
+        int m = mat[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == word[0] && dfs(mat, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    bool dfs(vector<vector<char>>& mat, string& word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= mat.size() || j < 0 || j >= mat[0].size() || mat[i][j] != word[index]) {
+            return false;
+        }
+
+        char temp = mat[i][j];
+        mat[i][j] = '*'; 
+
+        bool found = dfs(mat, word, i + 1, j, index + 1) ||
+                     dfs(mat, word, i - 1, j, index + 1) ||
+                     dfs(mat, word, i, j + 1, index + 1) ||
+                     dfs(mat, word, i, j - 1, index + 1);
+
+        mat[i][j] = temp; 
+        return found;
+    }
+};
