860e4562a5fd56864a6c4668fc43e9ae64523f03
VA-run23 <1523codes@gmail.com>
2025-02-19 15:45:26 +0530

    Added solution for Feb 19

--- February/POTD_19_02_2025.cpp
+++ February/POTD_19_02_2025.cpp
@@ -4,55 +4,52 @@ struct details{
     int len = 0;
     bool end = false;
 };
-
 class Solution {
-  public:
+    public:
     Node* mergeKLists(vector<Node*>& arr) {
         // Your code here
+        // int size = sizeof(arr) / sizeof(arr[0]);
         int size = arr.size();
         Node* firstHead = nullptr;
-        Node* last = nullptr;
+        Node* currSmall= NULL;
         int totalLen = 0;
+        Node* smallValPtr = arr[0];
         struct details info[size];
-
-        for (int i = 0; i < size; i++) {
+        for(int i  = 0; i<size; i++){
             info[i].curr = arr[i];
-            info[i].next = arr[i] ? arr[i]->next : nullptr;
-            Node* temp = arr[i];
-            while (temp != nullptr) {
+            info[i].next =arr[i] ->next;
+            Node* temp  = arr[i];
+            if(smallValPtr->data > arr[i]->data){
+                smallValPtr = arr[i];
+            }
+            while(temp != NULL){
                 info[i].len++;
                 temp = temp->next;
             }
-            totalLen += info[i].len;
+            totalLen+= info[i].len;
         }
-
-        for (int i = 0; i < totalLen; i++) {
-            Node* currSmall = nullptr;
+        ///MERGING: HERE:
+        for(int i  = 0; i<totalLen; i++){
+            currSmall = nullptr;
             int currentI = -1;
-
             for (int j = 0; j < size; j++) {
-                if (!info[j].end && (currSmall == nullptr || info[j].curr->data < currSmall->data)) {
+                if (!info[j].end && (currSmall == NULL || currSmall->data > info[j].curr->data)) {
                     currSmall = info[j].curr;
                     currentI = j;
                 }
             }
-
-            if (firstHead == nullptr) {
+            if(i==0){
                 firstHead = currSmall;
-                last = currSmall;
-            } else {
-                last->next = currSmall;
-                last = currSmall;
+            }else{
+              smallValPtr->next = currSmall;
             }
-
-            if (info[currentI].next == nullptr) {
+            smallValPtr = currSmall;         
+            if(info[currentI].curr->next == NULL) {
                 info[currentI].end = true;
             } else {
-                info[currentI].curr = info[currentI].next;
-                info[currentI].next = info[currentI].next->next;
+                info[currentI].curr = info[currentI].curr->next;
             }
         }
-
         return firstHead;
     }
 };
