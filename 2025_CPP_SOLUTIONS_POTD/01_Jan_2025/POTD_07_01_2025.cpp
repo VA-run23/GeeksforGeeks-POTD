1ca4c7a22a1dd6850fa9c1cf9a26de16d7d7cbd7
VA-run23 <1523codes@gmail.com>
2025-02-26 12:27:17 +0530

    Added solution for Jan 07

--- 2025_CPP_SOLUTIONS_POTD/01_Jan_2025/POTD_07_01_2025.cpp
+++ 2025_CPP_SOLUTIONS_POTD/01_Jan_2025/POTD_07_01_2025.cpp
@@ -11,11 +11,17 @@ class Solution {
             } else if ((arr[left] + arr[right]) > target) {
                 right--;
             } else { // equal condition
+                    // If the value at the left index is equal to the value at the right index
                 if (arr[left] == arr[right]) {
+                    // Calculate the count of elements between left and right indices (inclusive)
                     int count = right - left + 1;
+                    
+                    // Calculate the number of pairs that can be formed using the count of elements
                     pairs += (count * (count - 1)) / 2;
+                    
+                    // Break out of the loop since we have found equal elements at left and right indices
                     break;
-                } else {
+                }else {
                     int leftCount = 1, rightCount = 1;
                     while (left + 1 < right && arr[left] == arr[left + 1]) {
                         leftCount++;
