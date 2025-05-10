0b577263cbeb26cb18a733a5d8282ef9aeea8804
VA-run23 <1523codes@gmail.com>
2025-05-10 08:08:06 +0530

    Added solution for May 10

--- 2025_CPP_SOLUTIONS_POTD/05_May_2025/POTD_08_05_2025.cpp
+++ 2025_CPP_SOLUTIONS_POTD/05_May_2025/POTD_08_05_2025.cpp
@@ -1,12 +1,15 @@
 /*
-`
+    This function finds the missing number in an arithmetic progression.
+    The common difference d is determined from the first and last two elements.
+    Binary search is used to efficiently locate the missing number.
+    At each step, mid is calculated and checked against expected values.
+    If the progression holds, the search moves right; otherwise, it moves left.
+    The missing number is derived using the initial term and calculated index.
+    Time Complexity: O(log N), Space Complexity: O(1), where N is the array size.
 */
-
-
 class Solution {
   public:
     int findMissing(vector<int> &arr) {
-        // code here
         int n = arr.size();
         int x = arr[1] - arr[0], 
             y = arr[n-1] - arr[n-2], 
